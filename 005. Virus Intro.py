8a2d0bb58ea14a7710ba6fb5207ad64d6ff9b42a
MrRitzer <nmritter02@gmail.com>
2020-12-17 13:38:27 -0600

    Finished five
    Signed-off-by: MrRitzer <nmritter02@gmail.com>

--- 005. Virus Intro.py
+++ 005. Virus Intro.py
@@ -1,30 +1,11 @@
-import random
-def solution(x,y):
-    arrF = [int(x),int(y)]
-    M = 1
-    F = 1
-    gens1 = -1
-    gens2 = 0
-    arrI = [M,F]
-    while arrI != arrF:
-        arrI = [M,F]
-        x = random.randint(1,2)
-        if x == 1:
-            M = M + F
-            F = F
-            gens1 = gens1 + 1
-        if x == 2:
-            M = M
-            F = F + M
-            gens1 = gens1 + 1
-        if M > 10^50 or F > 10^50:
-            gens1=-1
-            M = 1
-            F = 1
-            gens2 = gens2 + 1
-        if gens2 > 10^50:
-            gens1 = 'impossible'
-            break
-
-    return str(gens1)
-print(solution ('0','1'))
+def solution(x, y):
+    x, y = int(x), int(y)
+    gens = 0
+    while (x != 1 and y != 1):
+        if x % y == 0:
+            return "impossible"
+        else:
+            gens = gens+int(max(x, y)/min(x, y))
+            x, y = max(x, y) % min(x, y), min(x, y)
+    return str(gens+max(x, y)-1)
+print(solution('4','7'))
