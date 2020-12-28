c05b4aba160d6b63a6c34ab7cda5cb31c80c545b
MrRitzer <nmritter02@gmail.com>
2020-12-28 11:03:40 -0600

    Finished 14
    Signed-off-by: MrRitzer <nmritter02@gmail.com>

---
+++ 14. List Remove Duplicates.py
@@ -0,0 +1,7 @@
+def rmver(listx = []):
+    listx = set(listx)
+    listx = list(listx)
+    return listx
+list1 = ["Snack","Car","Car","Walleye","Boat","Boat"]
+list2 = [1,2,3,4,5,5,3,6]
+print(rmver(list1))
