940d9a9be3717c6c72b8682e76fb5063cadc984e
MrRitzer <nmritter02@gmail.com>
2021-01-06 14:39:41 -0600

    Cleaned up Repo
    Signed-off-by: MrRitzer <nmritter02@gmail.com>

--- Nelly/01. Tic Tac Toe.py
+++
@@ -1,4 +0,0 @@
-import random
-x = [(random.randint(0,1)) for k in range(0,3)]
-y = [(random.randint(0,1)) for k in range(0,3)]
-z = [(random.randint(0,1)) for k in range(0,3)]
--- Other/00001. Woman.py
+++
@@ -1,6 +0,0 @@
-import random
-x = random.randint(1,2)
-if x == 1:
-    print("Go find woman")
-elif x == 2:
-    print("Go find hobby")
